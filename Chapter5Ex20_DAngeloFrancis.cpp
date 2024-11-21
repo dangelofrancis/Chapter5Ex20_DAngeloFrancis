0e57ee1f25efbd3899f9f4c205add6d021d241a1
D'Angelo Francis <dangelofrancis9@gmail.com>
2024-11-20 19:30:58 -0500

    made the program repeatable.

--- Chapter5Ex20_DAngeloFrancis.cpp
+++ Chapter5Ex20_DAngeloFrancis.cpp
@@ -1,20 +1,95 @@
-// Chapter5Ex20_DAngeloFrancis.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+TITLE: Chapter 5 Exercise 20 - Random Number Guessing Game
+FILE NAME: Chapter5Ex20_DAngeloFrancis.cpp
+PROGRAMMER: D'Angelo Francis
+DATE: November 2024
+REQUIREMENTS:
+Write a program that generates a random number and asks the user to guess what the number is.
+
+If the user’s guess is higher than the random number, the program should display “Too high, try again.”
+
+If the user’s guess is lower than the random number, the program should display “Too low, try again.”
+
+The program should use a loop that repeats until the user correctly guesses the random number.*/
 
 #include <iostream>
+#include <random>
+using namespace std;
+
+random_device numberEngine;
+uniform_int_distribution<int> randomInt(0, 100);
+
+int GetRandomNumber();
+int GuessNumber();
+void HighOrLow(int number, int guess);
 
 int main()
-{
-    std::cout << "Hello World!\n";
+{	
+	int choice = 0;
+
+	cout << "Random Number Guessing Game\n";
+	cout << "___________________________\n\n";
+	cout << "1. PLAY\n";
+	cout << "2. QUIT\n\n";
+
+	do
+	{
+		cout << "Enter your choice (1 or 2): ";
+		cin >> choice;
+
+		if (choice < 1 || choice > 2)
+		{
+			cout << "\n*ERROR* Invalid choice! Try again.\n";
+		}
+	} while (choice < 1 || choice > 2);
+
+	while (choice == 1)
+	{
+		int number = GetRandomNumber();
+		int guess;
+
+		do
+		{
+			guess = GuessNumber();
+			HighOrLow(number, guess);
+		} while (number != guess);
+
+		cout << "\nCongrats! You guessed the number correctly.\n";
+		cout << "Play again?\n\n";
+		
+		do
+		{
+			cout << "(Enter 1 for YES, or 2 for NO): ";
+			cin >> choice;
+
+			if (choice < 1 || choice > 2)
+			{
+				cout << "\n*ERROR* Invalid choice! Try again.\n";
+			}
+		} while (choice < 1 || choice > 2);
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int GetRandomNumber()
+{
+	int number = randomInt(numberEngine);
+	return number;
+}
+int GuessNumber()
+{
+	int guess;
+	cout << "\nGuess the number: ";
+	cin >> guess;
+	return guess;
+}
+void HighOrLow(int number, int guess)
+{
+	if (guess > number)
+	{
+		cout << "\nToo high, try again!\n";
+	}
+	else if (guess < number)
+	{
+		cout << "\nToo low, try again!\n";
+	}
+}
